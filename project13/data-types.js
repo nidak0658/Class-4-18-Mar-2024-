5b2d10009245e370ea124b4926a69f90a4a8708b
nidak0658 <165899145+nidak0658@users.noreply.github.com>
2024-04-03 23:12:42 +0500

    Add files via upload

---
+++ project13/data-types.js
@@ -0,0 +1,44 @@
+// part 1 sir code
+/*
+let myName: string = "Isfhan Ahmed";
+myName = "The Wolf";
+console.log(myName.toUpperCase()); // THE WOLF
+
+
+let myAge: number = 25;
+myAge = 26.456;
+console.log(myAge.toFixed(2)); // (2) two times run 26.46 // (1) 26.5 one time run
+
+
+let isJobless: boolean = false;
+isJobless = true;
+
+
+// part 2 my code
+let myName: string = "Nida Khan";
+myName = "The Book";
+console.log(myName.toUpperCase()); // THE BOOK
+
+
+let myAge: number = 24;
+myAge = 14.404;
+console.log(myAge.toFixed(2)); // (2) two times run 14.40 // (1) 14.4 one time run
+
+
+let isJobless: boolean = false;
+isJobless = true;
+
+
+let myPlane;
+console.log(myPlane); // undefined
+
+
+let myCar: null = null;
+console.log(myCar);
+
+
+let a;
+console.log(a);
+*/
+var b;
+(b);
